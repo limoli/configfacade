e9fdf69c74add181f647b796ac9a1678c69a6dc2
Andrea Limoli <andrealimoli1992@gmail.com>
2018-11-02 11:24:13 +0100

    Fix viper instance.

--- viper/config.go
+++ viper/config.go
@@ -5,26 +5,29 @@ import (
 	"github.com/spf13/viper"
 )
 
-type Config struct{}
-
-func (c *Config) Get(key string) interface{} {
-	return viper.Get(key)
+type Config struct {
+	instance *viper.Viper
 }
 
 func (c *Config) LoadFile(path, filename, extension string) error {
-	viper.AddConfigPath(path)
-	viper.SetConfigName(filename)
-	viper.SetConfigType(extension)
-	return viper.ReadInConfig()
+	c.instance = viper.New()
+	c.instance.AddConfigPath(path)
+	c.instance.SetConfigName(filename)
+	c.instance.SetConfigType(extension)
+	return c.instance.ReadInConfig()
 }
 
 func (c *Config) LoadEnvVars(vars []configfacade.EnvVar) error {
 	var err error
 	for _, v := range vars {
-		err = viper.BindEnv(v.Key, v.Env)
+		err = c.instance.BindEnv(v.Key, v.Env)
 		if err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func (c *Config) Get(key string) interface{} {
+	return c.instance.Get(key)
 }
