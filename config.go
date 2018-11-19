6c14b756948e04231571ab127a4a73a0ea396251
Andrea Limoli <andrealimoli1992@gmail.com>
2018-11-19 11:15:23 +0100

    Add documentation

--- config.go
+++ config.go
@@ -1,11 +1,13 @@
 package configfacade
 
+// Config is the interface to implement to make compatible library
 type Config interface {
 	LoadFile(path string, filename string, extension string) error
 	LoadEnvVars(vars map[string]string) error
 	Get(key string) interface{}
 }
 
+// Settings is a structure containing the main information about the configuration file and environment variables
 type Settings struct {
 	Path      string
 	Name      string
@@ -13,6 +15,7 @@ type Settings struct {
 	EnvVars   map[string]string
 }
 
+// Init initialises the library instance with settings
 func Init(c Config, s Settings) (Config, error) {
 	var err error
 
